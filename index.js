3e9ee7d735754046c011a9b29452b43c20e430e8
DuhItsJay <suyash.kushwah27@gmail.com>
2022-02-26 23:42:21 -0600

    Format using Prettier

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ process.title = 'Hypixel Discord Chat Bridge'
 const app = require('./src/Application')
 
 app
-  .register()
-  .then(() => {
-    app.connect()
-  })
-  .catch(err => {
-    console.error(err)
-  })
+	.register()
+	.then(() => {
+		app.connect()
+	})
+	.catch(err => {
+		console.error(err)
+	})
