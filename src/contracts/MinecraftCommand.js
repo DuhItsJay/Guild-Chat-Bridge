506a7f7848ad1038688b472b7032f2dd7607f21d
DuhItsJay <suyash.kushwah27@gmail.com>
2022-03-11 00:27:35 -0600

    api routing on parent class

--- src/contracts/MinecraftCommand.js
+++ src/contracts/MinecraftCommand.js
@@ -1,25 +1,51 @@
+const axios = require('axios')
+const URL = require('url').URL
+
+const HYPIXEL_API = 'https://api.hypixel.net'
+const PROFILE_ROUTE = HYPIXEL_API + '/skyblock/profiles'
+
 class MinecraftCommand {
-  constructor(minecraft) {
-    this.minecraft = minecraft
-  }
+	constructor(minecraft) {
+		this.minecraft = minecraft
+	}
 
-  getArgs(message) {
-    let args = message.split(' ')
+	getArgs(message) {
+		let args = message.split(' ')
 
-    args.shift()
+		args.shift()
 
-    return args
-  }
+		return args
+	}
 
-  send(message) {
-    if (this.minecraft.bot.player !== undefined) {
-      this.minecraft.bot.chat(message)
-    }
-  }
+	send(message) {
+		if (this.minecraft.bot.player !== undefined) {
+			this.minecraft.bot.chat(message)
+		}
+	}
 
-  onCommand(player, message) {
-    throw new Error('Command onCommand method is not implemented yet!')
-  }
+	onCommand(player, message) {
+		throw new Error('Command onCommand method is not implemented yet!')
+	}
+
+	sortByLatest(data) {
+		return data.sort((a, b) => b.members[uuid].last_save - a.members[uuid].last_save)[0]
+	}
+
+	async getUUID(username) {
+		const { data } = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+
+		return data.hasOwnProperty('id') ? data.id : 'terminate'
+	}
+
+	async fetchRequest(uuid) {
+		const url = new URL(PROFILE_ROUTE)
+		url.searchParams.append('key', this.minecraft.app.config.api.key)
+		url.searchParams.append('uuid', uuid)
+
+		let { data } = await axios(url.toString())
+
+		return data
+	}
 }
 
 module.exports = MinecraftCommand
