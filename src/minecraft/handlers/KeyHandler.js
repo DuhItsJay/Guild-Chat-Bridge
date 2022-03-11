3d1ac0317e3aa0b3d4ea4f558bd3e6daf39dd6f3
DuhItsJay <suyash.kushwah27@gmail.com>
2022-03-10 23:53:17 -0600

    error handling and updates for api key

---
+++ src/minecraft/handlers/KeyHandler.js
@@ -0,0 +1,35 @@
+const EventHandler = require('../../contracts/EventHandler')
+const axios = require('axios')
+
+class KeyHandler extends EventHandler {
+	constructor(minecraft, bot) {
+		super()
+
+		this.minecraft = minecraft
+	}
+
+	async registerEvents() {
+		setTimeout(() => {
+			this.onStatus()
+		}, 10000)
+	}
+
+	fetchAPIKey(error) {
+		if (this.minecraft.app.config.api.autoReplace && error.response.status == 403) {
+			this.minecraft.app.log.broadcast(`Replacing API Key`, 'Minecraft')
+			return this.minecraft.bot.chat(`/api new`)
+		}
+		if (error.response.status == 429) {
+			return this.minecraft.app.log.error(error.response.data.global ? 'Global Throttle has been placed' : 'Key limit reached')
+		}
+		return this.minecraft.app.log.error('API Key cannot be replaced => autoReplace is set to false')
+	}
+
+	async onStatus() {
+		await axios.get(`https://api.hypixel.net/key?key=${this.minecraft.app.config.api.key}`).catch(error => {
+			this.fetchAPIKey(error)
+		})
+	}
+}
+
+module.exports = KeyHandler
